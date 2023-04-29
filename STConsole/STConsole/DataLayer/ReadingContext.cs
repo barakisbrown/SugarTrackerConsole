369c3b4bc8252dbc96911bb426edbc5d126d536e
barakisbrown <lokislayer@gmail.com>
2023-04-28 21:44:30 -0500

    Data Layer

---
+++ STConsole/STConsole/DataLayer/ReadingContext.cs
@@ -0,0 +1,20 @@
+﻿namespace STConsole.DataLayer;
+
+using Microsoft.EntityFrameworkCore;
+using STConsole.Model;
+
+public class ReadingContext : DbContext
+{
+    public DbSet<Reading> Readings { get; set; }
+    public string dbPath;
+
+    public ReadingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        dbPath = System.IO.Path.Join(path, "readings.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={dbPath}");
+
+}
