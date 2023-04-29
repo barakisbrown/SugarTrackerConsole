5abd5500c1ca0ae30ac55fb8189fe80c5744d036
barakisbrown <lokislayer@gmail.com>
2023-04-29 00:55:34 -0500

    Add toString()

--- STConsole/STConsole/Model/Reading.cs
+++ STConsole/STConsole/Model/Reading.cs
@@ -5,4 +5,9 @@ public class Reading
     public int Id { get; set; }
     public int Amount { get; set; }
     public DateTime Added { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID = {Id}, Amount = {Amount}, Date Added = {Added.ToShortDateString()}";
+    }    
 }
