b60c2e3264bdcbfa987b6b7dbabac4014a67f679
barakisbrown <lokislayer@gmail.com>
2023-04-29 20:05:53 -0500

    Added is working perfactly.

--- STConsole/STConsole/UserInput/Menu.cs
+++ STConsole/STConsole/UserInput/Menu.cs
@@ -1,8 +1,12 @@
-﻿namespace STConsole.UserInput;
+﻿using Serilog;
+using STConsole.DataLayer;
+using STConsole.Model;
+
+namespace STConsole.UserInput;
 
 public class Menu
 {
-    private readonly static string MenuInputString = "Please Select (1-4) OR 0 to exit";
+    private readonly static string MenuInputString = "Please Select (1-5) OR 0 to exit";
 
     public static void GetMenu()
     {
@@ -39,13 +43,41 @@ public class Menu
             catch (FormatException _)
             {
                 Console.WriteLine();
-                Console.WriteLine("Input needs to be between 0 and 4 only. Please try again.");
+                Console.WriteLine("Input needs to be between 0 and 5 only. Please try again.");
                 Console.Write(MenuInputString);
             }
         }
     }
 
-    public static void Add() { }
+    public static void Add() 
+    {        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Adding a new blood sugar reading.");
+            int amount = Input.GetAmount();
+            DateTime date = Input.GetDate();
+            Log.Debug("Blood Sugar Amount => {0}", amount);
+            Log.Debug("Date Added => {date}", date.ToShortDateString());
+            Reading reading = new() { Amount = amount, Added = date };
+            if (ReadingController.Insert(reading))
+            {
+                Console.WriteLine("Blood Sugar Reading has been successfully Added.");
+                Log.Debug("Row was inserted successfully");
+            }
+            else
+            {
+                Console.WriteLine("Blood Suagr was not successfully not added.");
+                Log.Error("Something happened where the log could not been inserted");
+            }
+
+            Console.WriteLine("Do you wish to add another? (Y/N)");
+            if (!Input.GetYesno())
+                break;
+        }
+        Thread.Sleep(1000);
+        Console.Clear();
+    }
     public static void Update() { }
     public static void Delete() { }
     public static void ShowAll() { }
