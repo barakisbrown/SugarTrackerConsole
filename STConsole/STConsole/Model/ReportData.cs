6876db9431e290550e48e2f845854eed3c0b4b75
barakisbrown <lokislayer@gmail.com>
2023-04-30 02:08:09 -0500

    Simple Class to help with reporting

---
+++ STConsole/STConsole/Model/ReportData.cs
@@ -0,0 +1,9 @@
+﻿namespace STConsole.Model;
+
+public class ReportData
+{
+    public int MIN { get; set; }
+    public int MAX { get; set; }
+    public int AVG { get; set; }
+    public int Over200 { get; set; }
+}
