dc8397fa0c4729cb6fb71990da540e406f48e03c
barakisbrown <lokislayer@gmail.com>
2023-04-30 02:07:48 -0500

    Add GetReportData()

--- STConsole/STConsole/DataLayer/ReadingController.cs
+++ STConsole/STConsole/DataLayer/ReadingController.cs
@@ -2,6 +2,7 @@
 
 using ConsoleTableExt;
 using STConsole.Model;
+using System;
 
 public class ReadingController
 {
@@ -40,7 +41,20 @@ public class ReadingController
             .WithTitle("Blood Sugar Readings", ConsoleColor.Yellow, ConsoleColor.DarkGray)
             .WithTextAlignment(new Dictionary<int, TextAligntment>
             {
+                {1, TextAligntment.Center },
                 {2, TextAligntment.Center }
             }).ExportAndWriteLine();
+    }
+
+    public static ReportData GetReportData()
+    {
+        using var _context = new ReadingContext();
+
+        int max = _context.Readings.Max(x => x.Amount);
+        int min = _context.Readings.Min(x => x.Amount);
+        int avg = (int)_context.Readings.Average(x => x.Amount);
+        int ovr = _context.Readings.Count(x => x.Amount > 200);
+
+        return new ReportData { MAX = max, MIN = min, AVG = avg, Over200 = ovr };
     }
 }
