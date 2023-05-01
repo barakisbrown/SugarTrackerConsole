64cf6260e90ef4ee3daef1f5dca89b1072c4e564
barakisbrown <lokislayer@gmail.com>
2023-05-01 18:32:35 -0500

    Changed to DateOnly

--- STConsole/STConsole/Model/Reading.cs
+++ STConsole/STConsole/Model/Reading.cs
@@ -4,7 +4,7 @@ public class Reading
 {
     public int Id { get; set; }
     public int Amount { get; set; }
-    public DateTime Added { get; set; }
+    public DateOnly Added { get; set; }
 
     public override string ToString()
     {
